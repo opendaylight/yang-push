adf2f01a24a9e5e6e5163ceb1a5fe0e8cb93d690
tripaam <ambtripa@cisco.com>
2015-09-21 17:12:48 +0530

    initial commit
    Change-Id: I49d91ee503911401f09618dc5ec8bf438197cc34
    Signed-off-by: tripaam <ambtripa@cisco.com>

---
+++ impl/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/yangpush/impl/rev141210/YangpushModule.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright © 2015 Copyright (c) 2015 cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.yangpush.impl.rev141210;
+
+import org.opendaylight.yangpush.impl.YangpushProvider;
+
+public class YangpushModule extends org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.yangpush.impl.rev141210.AbstractYangpushModule {
+    public YangpushModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
+        super(identifier, dependencyResolver);
+    }
+
+    public YangpushModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver, org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.yangpush.impl.rev141210.YangpushModule oldModule, java.lang.AutoCloseable oldInstance) {
+        super(identifier, dependencyResolver, oldModule, oldInstance);
+    }
+
+    @Override
+    public void customValidation() {
+        // add custom validation form module attributes here.
+    }
+
+    @Override
+    public java.lang.AutoCloseable createInstance() {
+        YangpushProvider provider = new YangpushProvider();
+        getBrokerDependency().registerProvider(provider);
+        return provider;
+    }
+
+}
